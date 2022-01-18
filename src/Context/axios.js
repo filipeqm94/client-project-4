fe8344adb53357ddc03ab86abb6eb27822b147e1
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-18 15:57:48 -0500

    🛠Fix git conflicts

--- src/Context/axios.js
+++ src/Context/axios.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 const baseURL = process.env.REACT_APP_DB_URL
 
-const baseURL = process.env.REACT_APP_DB_URL;
-
 const axiosInstance = axios.create({
   baseURL: baseURL,
   headers: {
