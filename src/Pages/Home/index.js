ca3d781a5de6646f40f3a774a082ea737a32c958
v-wang <victorwang920@gmail.com>
2022-01-24 01:03:00 -0500

    websocket stuff

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Navbar from '../Navbar';
 import ChatArea from '../ChatArea';
 import SideBar from '../SideBar';
+import { useState } from 'react';
 
 function Home() {
+  const [chatSocket, setChatSocket] = useState();
   return (
     <div className="main">
       <Navbar />
-      <ChatArea />
-      <SideBar />
+      <ChatArea chatSocket={chatSocket} setChatSocket={setChatSocket} />
+      <SideBar chatSocket={chatSocket} setChatSocket={setChatSocket} />
     </div>
   );
 }
--- src/Pages/SideBar/index.js
+++ src/Pages/SideBar/index.js
@@ -1,30 +1,25 @@
-import { useState } from 'react';
+import { useAuthState } from '../../Context/context';
 
-import { useAuthState, useAuthDispatch } from '../../Context/context';
-import { setSocket } from '../../Context/actions';
-
-function SideBar() {
-  const [chatSocket, setChatSocket] = useState();
-
+function SideBar({ setChatSocket, chatSocket }) {
   const { username, usersList } = useAuthState();
-  const dispatch = useAuthDispatch();
 
   function handleClick(targetUser) {
     // if there is an existing socket, close it
     if (chatSocket) {
-      console.log('should only see this if socket is open', chatSocket);
+      console.log('closing socket ---->', chatSocket);
       chatSocket.close();
     }
 
     // formulate room name
     const chat_room_name = [username, targetUser].sort().join('_');
-    const socketUrl = process.env.REACT_APP_WS_URL + chat_room_name + '/';
+    const newChatSocket = new WebSocket(
+      process.env.REACT_APP_WS_URL + chat_room_name + '/'
+    );
 
-    // set socket with formulated room name
-    setChatSocket(new WebSocket(socketUrl));
-
-    // send socket to context state
-    setSocket(dispatch, chatSocket);
+    setChatSocket(newChatSocket);
+    newChatSocket.addEventListener('open', (event) => {
+      newChatSocket.send(JSON.stringify({ type: 'open_chat' }));
+    });
   }
 
   return (
