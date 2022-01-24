9256bc4d5684678bc62cdce8ca95ca290654368c
filipeqm94 <queiroz.filipe94@gmail.com>
2022-01-24 03:46:44 -0500

    we have messages!!!!!

--- src/Pages/ChatArea/Chat/index.js
+++ src/Pages/ChatArea/Chat/index.js
@@ -1,11 +1,14 @@
-import './styles.css'
+import './styles.css';
 
-function Chat({ messages, user }) {
+function Chat({ messages, user, id }) {
   return (
     <div className="chat-msg-container">
-      {messages.map((message) => {
+      {messages.map((message, index) => {
         return (
-          <div className={user === message.user ? "sender" : "receiver"}>
+          <div
+            className={id === message.sender ? 'sender' : 'receiver'}
+            key={index}
+          >
             <h3>{message.user}:</h3>
             <p>{message.message}</p>
           </div>
