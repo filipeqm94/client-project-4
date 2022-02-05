ac7cfe04e39e0b69cbec8acff30bb3eb5b668818
v-wang <victorwang920@gmail.com>
2022-02-05 00:14:31 -0500

    restyled login page

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,3 +1,4 @@
+import './styles.scss';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuthState, useAuthDispatch } from '../../Context/context';
@@ -5,7 +6,7 @@ import { login } from '../../Context';
 
 function Login(props) {
   const [loginForm, setLoginForm] = useState({ username: '', password: '' });
-  
+
   const dispatch = useAuthDispatch();
   const { loading, errorMessage } = useAuthState();
 
@@ -23,34 +24,46 @@ function Login(props) {
   }
 
   return (
-    <div>
-      Login
-      {loading ? <p>Loading</p> : null}
-      <p>{errorMessage ? errorMessage : null}</p>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            name="username"
-            type="text"
-            value={loginForm.username}
-            onChange={handleLogin}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            name="password"
-            type="password"
-            value={loginForm.password}
-            onChange={handleLogin}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-      <p>
-        Don't have an account? <Link to="/signup">Sign Up</Link>
-      </p>
+    <div className="login-container">
+      <div className="login-area">
+        <div className="login-area-top">
+          <h1>
+            Welcome to <em>Chatter</em>
+          </h1>
+          <p>
+            Learning a new language? Practice by chatting with proficient
+            speakers and make new connections along the way!
+          </p>
+        </div>
+        Login
+        {loading ? <p>Loading</p> : null}
+        <p>{errorMessage ? errorMessage : null}</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input
+              name="username"
+              type="text"
+              value={loginForm.username}
+              onChange={handleLogin}
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              name="password"
+              type="password"
+              value={loginForm.password}
+              onChange={handleLogin}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        <p>
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </p>
+      </div>
+      <div className="content-area"></div>
     </div>
   );
 }
