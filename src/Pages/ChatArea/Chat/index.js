5588696461396c4a1fcd9894f8b398b6d863f3d9
v-wang <victorwang920@gmail.com>
2022-02-05 03:14:06 -0500

    added autoscroll to chat area

--- src/Pages/ChatArea/Chat/index.js
+++ src/Pages/ChatArea/Chat/index.js
@@ -1,18 +1,33 @@
 import './styles.scss';
+import { useEffect } from 'react';
 
 function Chat({ messages, id }) {
+  function scrollBottom() {
+    const msgContainer =
+      document.getElementsByClassName('chat-msg-container')[0];
+    msgContainer.scrollTop = msgContainer.scrollHeight;
+  }
+
+  useEffect(() => {
+    scrollBottom();
+  }, [messages]);
+
   return (
     <div className="chat-msg-container">
-      {messages ? messages.map((message, index) => {
-        return (
-          <div
-            className={`message ${id === message.sender ? 'sender' : 'receiver'}`}
-            key={index}
-          >
-            <p>{message.message}</p>
-          </div>
-        );
-      }) : null}
+      {messages
+        ? messages.map((message, index) => {
+            return (
+              <div
+                className={`message ${
+                  id === message.sender ? 'sender' : 'receiver'
+                }`}
+                key={index}
+              >
+                <p>{message.message}</p>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 }
