5ccd35bf4f79f95da272ec09904bdc794843de62
filipeqm94 <queiroz.filipe94@gmail.com>
2022-02-06 12:39:11 -0500

    get openned chats info

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 //components
+import { useEffect } from 'react';
 import AppRoute from './Components/AppRoute';
+import { useAuthState } from './Context';
+import axiosInstance from './Context/axios';
 
 function App() {
+  const { username } = useAuthState();
+
+  useEffect(() => {
+    axiosInstance
+      .get(`getchatrooms/?username=${username}`)
+      .then(console.log)
+      .catch(({ response }) => console.log(response));
+  }, []);
+
   return (
     <>
       <AppRoute />
