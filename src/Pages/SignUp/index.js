5a7addf31487a1dc6af55ea188278b6b8d8825fd
v-wang <victorwang920@gmail.com>
2022-02-06 17:33:11 -0500

    sign up page restyled

--- src/Pages/SignUp/index.js
+++ src/Pages/SignUp/index.js
@@ -4,6 +4,7 @@ import { useAuthDispatch, useAuthState } from '../../Context';
 import { signup } from '../../Context';
 
 import './styles.scss';
+import ghLogo from '../../assets/images/gh-logo.png';
 
 function SignUp() {
   const [signupForm, setSignupForm] = useState({
@@ -57,90 +58,119 @@ function SignUp() {
   }
 
   return (
-    <div>
-      <h2>SignUp</h2>
-      {errorMessage ? errorMessage : null}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            required
-            maxLength="16"
-            value={signupForm.username}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="password"
-            name="password"
-            required
-            minLength="8"
-            value={signupForm.password}
-            onChange={handleChange}
-          />
-        </label>
-        {signupForm.password !== signupForm.confirmPwd ? (
-          <p>Passwords must match</p>
-        ) : null}
-        <label>
-          Confirm Password:
-          <input
-            type="password"
-            name="confirmPwd"
-            required
-            value={signupForm.confirmPwd}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            required
-            value={signupForm.email}
-            onChange={handleChange}
-          />
-        </label>
-        {signupForm.email !== signupForm.confirmEmail ? (
-          <p>Emails must match</p>
-        ) : null}
-        <label>
-          Confirm Email:
-          <input
-            type="email"
-            name="confirmEmail"
-            required
-            value={signupForm.confirmEmail}
-            onChange={handleChange}
-          />
-        </label>
-        <label>
-          Primary Language:
-          <select onChange={handleChange} name="primaryLanguage" required>
-            <option value="">Please choose a language</option>
-            <option value="english">English</option>
-            <option value="spanish">Spanish</option>
-            <option value="french">French</option>
-            <option value="portuguese">Portuguese</option>
-          </select>
-        </label>
-        <label>
-          Learning Language:
-          <select onChange={handleChange} name="learningLanguage" required>
-            <option value="">Please choose a language</option>
-            <option value="english">English</option>
-            <option value="spanish">Spanish</option>
-            <option value="french">French</option>
-            <option value="portuguese">Portuguese</option>
-          </select>
-        </label>
-        <input type="submit" value="Sign Up" />
-      </form>
+    <div className="login-container">
+      <div className="login-side">
+        <div className="login-side-top">
+          <h1>
+            Sign up with <em>Chatter</em>
+          </h1>
+          <p>
+            Create an account to get started. Your Primary Language will be the
+            language you're most comfortabe with.
+          </p>
+          <div className="login-area">
+            <form onSubmit={handleSubmit}>
+              <label>
+                Username:
+                <input
+                  type="text"
+                  name="username"
+                  required
+                  maxLength="16"
+                  value={signupForm.username}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Password:
+                <input
+                  type="password"
+                  name="password"
+                  required
+                  minLength="8"
+                  value={signupForm.password}
+                  onChange={handleChange}
+                />
+              </label>
+              {signupForm.password !== signupForm.confirmPwd ? (
+                <p>Passwords must match</p>
+              ) : null}
+              <label>
+                Confirm Password:
+                <input
+                  type="password"
+                  name="confirmPwd"
+                  required
+                  value={signupForm.confirmPwd}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  required
+                  value={signupForm.email}
+                  onChange={handleChange}
+                />
+              </label>
+              {signupForm.email !== signupForm.confirmEmail ? (
+                <p>Emails must match</p>
+              ) : null}
+              <label>
+                Confirm Email:
+                <input
+                  type="email"
+                  name="confirmEmail"
+                  required
+                  value={signupForm.confirmEmail}
+                  onChange={handleChange}
+                />
+              </label>
+              <label>
+                Primary Language:
+                <select onChange={handleChange} name="primaryLanguage" required>
+                  <option value="">Please choose a language</option>
+                  <option value="english">English</option>
+                  <option value="spanish">Spanish</option>
+                  <option value="french">French</option>
+                  <option value="portuguese">Portuguese</option>
+                </select>
+              </label>
+              <label>
+                Learning Language:
+                <select
+                  onChange={handleChange}
+                  name="learningLanguage"
+                  required
+                >
+                  <option value="">Please choose a language</option>
+                  <option value="english">English</option>
+                  <option value="spanish">Spanish</option>
+                  <option value="french">French</option>
+                  <option value="portuguese">Portuguese</option>
+                </select>
+              </label>
+              <input type="submit" value="Sign Up" />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="content-side">
+        <div className="screen-shot-area"></div>
+        <footer>
+          <div>
+            <a
+              href="https://github.com/filipeqm94/client-project-4"
+              target=" _blank"
+            >
+              <img src={ghLogo} alt="GitHub Logo" />
+              <h4>source code</h4>
+            </a>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
